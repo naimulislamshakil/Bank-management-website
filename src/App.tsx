b216b3e12e056c2a7caf7158da93feda16ee5909
Naimul Islam <ms0413354@gmail.com>
2023-02-06 23:01:32 +0600

    Add register from

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
 import Navbar from './Components/Navbar';
+import Register from './Pages/Register';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
