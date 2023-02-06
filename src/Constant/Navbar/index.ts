39ad35e6d135317879cc1cf2f94e4892a67489d8
Naimul Islam <ms0413354@gmail.com>
2023-02-06 20:38:57 +0600

    Add some page, component and conast file

---
+++ src/Constant/Navbar/index.ts
@@ -0,0 +1,22 @@
+export const Navbar_Item = [
+	{
+		id: 1,
+		text: 'Home',
+		route: '/',
+	},
+	{
+		id: 2,
+		text: 'LogIn',
+		route: '/login',
+	},
+	{
+		id: 3,
+		text: 'Register',
+		route: '/register',
+	},
+	{
+		id: 4,
+		text: 'Dashboard',
+		route: '/dashboard',
+	},
+];
