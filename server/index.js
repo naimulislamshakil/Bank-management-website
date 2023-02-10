e14b691a304ea915f28ce61ed3ecb49de98575d3
Naimul Islam <ms0413354@gmail.com>
2023-02-10 09:22:47 +0600

    Add node project in mongoose compass

--- server/index.js
+++ server/index.js
@@ -6,20 +6,21 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+// connect mongoose
+mongoose
+	.connect('mongodb://127.0.0.1:27017:27017/test', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Database Connected Successfully!'.yellow.bold))
+	.catch((err) => console.log(err));
+
 // Add medialeware
 app.use(cors());
 app.use(express.json());
 
 // get route
 const RegisterRoute = require('./Routes/v1/Register.route');
-
-// connect mongoose
-mongoose
-	.connect('mongodb://localhost:27017/', {
-		useNewUrlParser: 'true',
-	})
-	.then((res) => console.log('Database Already Added Successfull!'.yellow.bold))
-	.catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
 	res.send(`<h1>How are you?</h1>`);
